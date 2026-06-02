b9e98a8352d43f0ed87b915b939f0166c047361c
salauddeen680 <salauddeen680@gmail.com>
2026-06-03 00:38:45 +0530

    Create page.tsx

---
+++ app/app/admin/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Admin() {
+  return (
+    <div style={{ color: "white", padding: 20 }}>
+      <h1>CCU ADMIN PANEL</h1>
+      <p>Manage Comics & Characters</p>
+    </div>
+  )
+}
