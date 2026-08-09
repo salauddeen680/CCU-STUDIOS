c392b513c35b63419e02ae00bec3c8e2484ddaec
salauddeen680 <salauddeen680@gmail.com>
2026-08-09 11:51:42 +0530

    Add service worker for caching assets

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'ccu-cache-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(['/']);
+  }));
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+  }));
+});
