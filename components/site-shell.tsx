24c6969700ce17b63d23febca670d18c1a5016d8
salauddeen680 <salauddeen680@gmail.com>
2026-08-15 00:16:27 +0530

    Add PageTracker component to SiteShell

--- components/site-shell.tsx
+++ components/site-shell.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./header"
 import { Footer } from "./footer"
+import PageTracker from "./PageTracker"
 
 export function SiteShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="cosmic-bg flex min-h-screen flex-col">
+      {/* 📡 Background Traffic & Analytics Engine */}
+      <PageTracker />
+      
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
